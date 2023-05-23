aebd8dca3c0ca794db03a60865b09c6c0beeb88f
Omar-Mohsin <Omarabdessalam4748@gmail.com>
2023-05-23 22:49:45 +0300

    adding a new counter and make a counter count to 5

--- Main.java
+++ Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        int counter  = 0;
+        for (int i =0 ; i<5;i++){
+            counter ++;
+        }
+
+        System.out.println(counter);
     }
 }
